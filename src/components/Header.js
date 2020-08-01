7ab0f7d5cd1f3240ccb8ecc2f0b114d49f1a7e88
Barbara Kumor <b.kumor@yahoo.com>
2020-08-01 18:26:49 +0200

    upload component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className="header container">
             <h1 className="title">ancient<span className="subtitle">GALLERY</span></h1>
         </header>
     )
